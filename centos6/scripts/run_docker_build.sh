902b568a8ea6b79776d1822c8e20746961c8a183
shadow_walker <shadowwalkersb@gmail.com>
2017-04-02 21:37:57 -0500

    check cmd-line args

--- centos6/scripts/run_docker_build.sh
+++ centos6/scripts/run_docker_build.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 
+if [ $# -ne 2 ];then
+    echo
+    echo -e '\033[35m'"  Usage: $(basename ${0}) [docker-image] [workspace-root-dir]"'\033[0m'
+    echo
+    exit 1
+fi
+
 docker_image=$1
+root_dir=$(cd $2; pwd -P)
+
 sudo docker info
 
 cat << EOF | sudo docker run -i \
-                        -v $HOME/workspace:/workspace \
+                        -v "$root_dir":/workspace \
                         -a stdin -a stdout -a stderr \
                         $docker_image \
                         bash
