43df380eeb2ab4ec9b500b1fe1b54559e05f31b9
shadow_walker <shadowwalkersb@gmail.com>
2017-04-10 00:13:56 -0500

    move docker_script.sh contents into run_docker_build.sh

--- centos6/scripts/run_docker_build.sh
+++ centos6/scripts/run_docker_build.sh
@@ -18,6 +18,14 @@ cat << EOF | sudo docker run -i \
                         $docker_image \
                         bash
 
-bash /workspace/eman2/docker/docker_script.sh
+EMAN_REPO_DIR="/workspace/eman2"
+OUTPUT_DIR="/workspace/centos6"
+CONSTRUCT_YAML_DIR="/workspace/build-scripts/constructor"
+
+set -x
+export PYTHONUNBUFFERED=1
+
+bash dockerfile.sh
+bash build_and_package.sh ${EMAN_REPO_DIR} ${OUTPUT_DIR} ${CONSTRUCT_YAML_DIR}
 
 EOF
