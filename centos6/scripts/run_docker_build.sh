7a42823777606446ebae7f4f7a93ce57d9f8ede2
shadow_walker <shadowwalkersb@gmail.com>
2017-04-10 00:15:23 -0500

    replace vars with their values

--- centos6/scripts/run_docker_build.sh
+++ centos6/scripts/run_docker_build.sh
@@ -18,14 +18,10 @@ cat << EOF | sudo docker run -i \
                         $docker_image \
                         bash
 
-EMAN_REPO_DIR="/workspace/eman2"
-OUTPUT_DIR="/workspace/centos6"
-CONSTRUCT_YAML_DIR="/workspace/build-scripts/constructor"
-
 set -x
 export PYTHONUNBUFFERED=1
 
 bash dockerfile.sh
-bash build_and_package.sh ${EMAN_REPO_DIR} ${OUTPUT_DIR} ${CONSTRUCT_YAML_DIR}
+bash build_and_package.sh /workspace/eman2 /workspace/centos6 /workspace/build-scripts/constructor
 
 EOF
