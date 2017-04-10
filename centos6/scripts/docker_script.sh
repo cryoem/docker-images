e864c7437dc3406f00ee5842413f6f59f69cf1b9
shadow_walker <shadowwalkersb@gmail.com>
2017-04-09 23:18:40 -0500

    gather var assignments at top of file

--- centos6/scripts/docker_script.sh
+++ centos6/scripts/docker_script.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+EMAN_REPO_DIR="/workspace/eman2"
+OUTPUT_DIR="/workspace/centos6"
+CONSTRUCT_YAML_DIR="/workspace/build-scripts/constructor"
+
 set -x
 export PYTHONUNBUFFERED=1
 
@@ -22,12 +26,9 @@ cd constructor-eman/
 python setup.py install
 
 # Build eman recipe
-EMAN_REPO_DIR="/workspace/eman2"
 conda build ${EMAN_REPO_DIR}/recipes/eman -c cryoem -c defaults -c conda-forge
 
 # Package eman
-OUTPUT_DIR="/workspace/centos6"
-CONSTRUCT_YAML_DIR="/workspace/build-scripts/constructor"
 mkdir -p ${OUTPUT_DIR} && cd ${OUTPUT_DIR}
 cp -a ${CONSTRUCT_YAML_DIR}/ .
 sed -i.bak "s~\(^.*file://\)\(.*$\)~\1${CONDA_PREFIX}/conda-bld/~" construct.yaml
