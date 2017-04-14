e03e8db4255673debdd5fd0c7f92c49cf61a0dc3
shadow_walker <shadowwalkersb@gmail.com>
2017-04-13 23:48:36 -0500

    mount cache

--- scripts/run_docker_build.sh
+++ scripts/run_docker_build.sh
@@ -15,12 +15,26 @@ scripts_root_dir=$(cd $(dirname $0)/..; pwd -P)
 docker_root_dir="/workspace"
 docker_scripts_root_dir="/scripts_root"
 
+dot_conda_dir=${root_dir}/docker_volumes/dot_conda
+docker_dot_conda_dir=/root/.conda/
+
+conda_dir=${root_dir}/docker_volumes/conda_dir
+
+conda_bld_dir=${conda_dir}/conda-bld
+docker_conda_bld_dir=/root/miniconda2/conda-bld
+
+pkgs_dir=${conda_dir}/pkgs
+docker_pkgs_dir=/root/miniconda2/pkgs
+
 
 docker info
 
 docker run -i \
             -v "$root_dir":"$docker_root_dir" \
             -v "$scripts_root_dir":"$docker_scripts_root_dir" \
+            -v "$dot_conda_dir":"$docker_dot_conda_dir" \
+            -v "$conda_bld_dir":"$docker_conda_bld_dir" \
+            -v "$pkgs_dir":"$docker_pkgs_dir" \
             -a stdin -a stdout -a stderr \
             $docker_image \
             bash << EOF 
