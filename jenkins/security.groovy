7472a448bce4907ba06079605e9a58f27f4c8ccb
EMAN Build <eman2@localhost.localdomain>
2019-08-04 17:18:15 -0500

    stash

--- jenkins/security.groovy
+++ jenkins/security.groovy
@@ -6,11 +6,8 @@ import jenkins.security.s2m.AdminWhitelistRule
 
 def instance = Jenkins.getInstance()
 
-def user = System.console().readLine 'Enter admin user name: '
-def pass = System.console().readPassword 'Enter admin passw: '
 
 def hudsonRealm = new HudsonPrivateSecurityRealm(false)
-hudsonRealm.createAccount(user, pass)
 instance.setSecurityRealm(hudsonRealm)
 
 def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
